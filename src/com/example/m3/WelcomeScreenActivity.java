56d771248395628404faec06eb2ce349025cbd28
Edwin Choate <edwin.choate@gmail.com>
2014-02-03 17:42:37 -0500

    Made minor changes to WelcomeScreenActivity.java

--- src/com/example/m3/WelcomeScreenActivity.java
+++ src/com/example/m3/WelcomeScreenActivity.java
@@ -1,7 +1,15 @@
 package com.example.m3;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class WelcomeScreenActivity extends Activity {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.activity_welcome);
+	}
+	
 }
