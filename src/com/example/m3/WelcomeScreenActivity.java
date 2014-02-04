75db8f1d3fbab2778e7c9ef4ab47a788ccc0cbf4
Edwin Choate <edwin.choate@gmail.com>
2014-02-03 20:08:49 -0500

    Played around with intents on MainActivity.java and WelcomeScreenActivity.java

--- src/com/example/m3/WelcomeScreenActivity.java
+++ src/com/example/m3/WelcomeScreenActivity.java
@@ -2,14 +2,21 @@ package com.example.m3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class WelcomeScreenActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.activity_welcome);
 	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	
 	
 }
