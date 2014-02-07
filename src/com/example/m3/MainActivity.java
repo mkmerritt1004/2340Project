a758e160c3878041b3bed578c39bfbd27eea9912
Kimi <kwolford3@gatech.edu>
2014-02-06 20:26:51 -0500

    Added and actionListener for the login button and outline for checking login in success

--- src/com/example/m3/MainActivity.java
+++ src/com/example/m3/MainActivity.java
@@ -3,13 +3,23 @@ package com.example.m3;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.content.Context;
+import android.content.Intent;
+
+
 
 public class MainActivity extends Activity {
+	
+	Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerOnButton();
     }
 
 
@@ -19,5 +29,25 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+	public void addListenerOnButton() {
+		 
+		final Context context = this;
+ 
+		loginButton = (Button) findViewById(R.id.loginButton);
+ 
+		loginButton.setOnClickListener(new OnClickListener() {
+ 
+			@Override
+			public void onClick(View arg0) {
+ 
+				//if(email.equals("admin"){
+				//	move to success screen
+				//}else{
+				//	signal to try again
+				//}
+			}
+ 
+		});
     
+}
 }
