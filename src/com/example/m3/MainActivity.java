85351920f891c52f31b0a9bcb243786b836027be
tripproberts <tripp@tripp-roberts.com>
2014-02-15 02:42:11 -0500

    Adds webapp API functionality for logging in

--- src/com/example/m3/MainActivity.java
+++ src/com/example/m3/MainActivity.java
@@ -1,7 +1,20 @@
 package com.example.m3;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -35,7 +48,7 @@ public class MainActivity extends Activity {
     }
     public void updateTextView(String newStr) {
 
-        TextView textView = (TextView) findViewById(R.id.textView1);
+        TextView textView = (TextView) findViewById(R.id.textView4);
         textView.setText(newStr);
     }
     
@@ -53,16 +66,85 @@ public class MainActivity extends Activity {
 				passEdit   = (EditText)findViewById(R.id.editText2);
 				String userEditStr = userEdit.getText().toString();
 				String passEditStr = passEdit.getText().toString();
-				if(userEditStr.equals("admin") && passEditStr.equals("pass123")){
-					Intent intent = new Intent(context, SuccessScreenActivity.class);
-					startActivity(intent); 
-				}
-				else{
-					updateTextView("Incorrect Username or Password. Try Again.");
-				}
+
+				new CheckLoginTask().execute(userEditStr, passEditStr);
+				
+				//Intent intent = new Intent(context, SuccessScreenActivity.class);
+				//startActivity(intent); 
 			}
  
 		});
     
-}
+	}
+	
+	class CheckLoginTask extends AsyncTask<String, Void, Boolean> {
+
+	    private Exception exception;
+
+	    protected Boolean doInBackground(String... inputs) {
+	    	HttpURLConnection urlConnection = null;
+	        try {
+				disableConnectionReuseIfNecessary();
+				String email = inputs[0];
+				String password = inputs[1];
+				String url = "http://intense-garden-9893.herokuapp.com/api/users/sign_in.json";
+	        	urlConnection = (HttpURLConnection) new URL(url).openConnection();
+				urlConnection.setDoOutput(true); //Triggers POST
+				urlConnection.setRequestProperty("Content-Type", "application/json");
+				urlConnection.setRequestProperty("Accept", "application/json");
+				urlConnection.setRequestMethod("POST");
+								
+				JSONObject user = new JSONObject();
+				JSONObject params = new JSONObject();
+			    try {
+					params.put("email",email);
+				    params.put("password",password);
+				    user.put("user", params);
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			    Log.v("JSON",user.toString());
+			    byte[] outputBytes = user.toString().getBytes("UTF8");
+			    
+				OutputStream os = urlConnection.getOutputStream();
+				os.write(outputBytes);
+				os.close();
+				TextView textView = (TextView) findViewById(R.id.textView1);
+				Log.v("Response Code", String.valueOf(urlConnection.getResponseCode()));
+				return urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK;
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return false;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return false;
+			}
+	    }
+
+	    protected void onPostExecute(Boolean result) {
+			if ( result ){
+				updateTextView("Hooray");
+				//Intent intent = new Intent(context, SuccessScreenActivity.class);
+				//startActivity(intent); 
+			}
+			else{
+				updateTextView("Incorrect Username or Password. Try Again.");
+			}
+	    }
+	    
+		/**
+		 * required in order to prevent issues in earlier Android version.
+		 */
+		@SuppressWarnings("deprecation")
+		private void disableConnectionReuseIfNecessary() {
+		    // see HttpURLConnection API doc
+		    if (Integer.parseInt(Build.VERSION.SDK) 
+		            < Build.VERSION_CODES.FROYO) {
+		        System.setProperty("http.keepAlive", "false");
+		    }
+		}
+	}
 }
