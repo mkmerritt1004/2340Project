bb598c04dc543cc56b73f066fc85b29e1b745fc2
laurenpalmore <ltpalmore@gmail.com>
2014-02-25 18:21:16 -0500

    Edited AccountsOverviewActivity

--- src/com/example/m3/AccountsOverviewActivity.java
+++ src/com/example/m3/AccountsOverviewActivity.java
@@ -1,8 +1,26 @@
 package com.example.m3;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -20,6 +38,7 @@ public class AccountsOverviewActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accounts_overview);
         layout = (LinearLayout) findViewById(R.id.activityAccountsOverview);
+        //adds all the accounts; need to update accounts
         String[] accounts = {"Account 1", "Account 2"};
         for (String account:accounts) {
         	Button button = new Button(this);
@@ -55,6 +74,66 @@ public class AccountsOverviewActivity extends Activity {
 			}
 		});
     
+	}
+	class createAccountButtonTask extends AsyncTask<String, Void, HttpResponse> {
+
+		private Context context;
+		private HttpResponse response;
+		
+		private createAccountButtonTask(Context context) {
+		    this.context = context.getApplicationContext();
+		}
+
+	    protected HttpResponse doInBackground(String... inputs) {
+	    	HttpClient httpclient = new DefaultHttpClient();
+	        HttpGet httpget = new HttpGet("http://intense-garden-9893.herokuapp.com/api/users/accounts.json");
+
+	        try {
+	            // Execute HTTP Post Request
+	            response = httpclient.execute(httpget);
+	            
+	        } catch (ClientProtocolException e) {
+	            // TODO Auto-generated catch block
+	            System.out.println("CPE"+e);
+	        } catch (IOException e) {
+	            // TODO Auto-generated catch block
+	            System.out.println("IOE"+e);
+	        }
+	        return response;
+	    }
+
+	    protected void onPostExecute(HttpResponse response) {
+			if ( response.getStatusLine().getStatusCode() == 201 ){
+				Intent intent = new Intent(context, SuccessScreenActivity.class);
+				startActivity(intent); 
+			}
+			else{
+				try {
+					//byte[] accountInfo =  EntityUtils.toByteArray(response.getEntity());
+					//for (byte accountName: accountInfo) {
+						//String accountNameStr = accountName.toString();
+					//}
+					String accountInfo = EntityUtils.toString(response.getEntity());
+					String[] temp = null;
+					int i = 0;
+					int j = 0;
+					while (i<accountInfo.length()){
+						//look at string at i
+						//if "}" then copy from j to i into temp[] i++ j=i
+						//else i++ 
+					}
+					//updateButtons()
+					//get display name
+					
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+	    }
 	}
 	
 }
