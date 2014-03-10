58bb0a8bbe450b5be0fc64ceb9cee1dabec2cb24
Edwin Choate <edwin.choate@gmail.com>
2014-03-10 17:20:30 -0400

    Created SpendingReportActivity.java and established its basic layout.

---
+++ src/com/example/m3/SpendingReportActivity.java
@@ -0,0 +1,16 @@
+package com.example.m3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SpendingReportActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_spending_report);
+	}
+	
+	
+	
+}
