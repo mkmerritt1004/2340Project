a7494da1b97fb19678e4f0aef7eec261365adaaa
Edwin Choate <edwin.choate@gmail.com>
2014-03-11 12:16:22 -0400

    Added new activity, ReportDateSelectActivity, and layout file, activity_report_date_select.xml

--- src/com/example/m3/ReportDateSelectActivity.java
+++ src/com/example/m3/ReportDateSelectActivity.java
@@ -3,14 +3,13 @@ package com.example.m3;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class SpendingReportActivity extends Activity {
-
-	@Override
+public class ReportDateSelectActivity extends Activity {
+	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_report_date_select);
+		
 	}
-	
 	
 	
 	
