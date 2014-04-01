d002c5e14d27eded5205daa7ff1961ddea0b44a2
Edwin Choate <edwin.choate@gmail.com>
2014-04-01 15:20:21 -0400

    LoginTask.java

--- src/com/example/m3/LoginTask.java
+++ src/com/example/m3/LoginTask.java
@@ -15,6 +15,12 @@ import org.apache.http.message.BasicNameValuePair;
 
 import android.os.AsyncTask;
 
+/**
+ * LoginTask class.
+ * 
+ * @author Tripp
+ * @version 1.0
+ */
 class LoginTask extends AsyncTask<String, Void, HttpResponse> {
 
 	/**
@@ -22,7 +28,7 @@ class LoginTask extends AsyncTask<String, Void, HttpResponse> {
 	 */
     private HttpResponse response;
 	
-
+    @Override
     protected HttpResponse doInBackground(String... inputs) {
     	HttpClient httpclient = new DefaultHttpClient();
         HttpPost httppost = new HttpPost(inputs[0] + "/users/sign_in.json");
@@ -38,11 +44,9 @@ class LoginTask extends AsyncTask<String, Void, HttpResponse> {
             response = httpclient.execute(httppost);
             
         } catch (ClientProtocolException e) {
-            // TODO Auto-generated catch block
-            System.out.println("CPE"+e);
+            System.out.println("CPE" + e);
         } catch (IOException e) {
-            // TODO Auto-generated catch block
-            System.out.println("IOE"+e);
+            System.out.println("IOE" + e);
         }
         return response;
     }
