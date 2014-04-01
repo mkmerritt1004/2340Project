75a311123961c066cdb82641d664cdbfd3a845c4
Edwin Choate <edwin.choate@gmail.com>
2014-04-01 15:13:08 -0400

    Made checkstyle changes to DeposityActivity.java

--- src/com/example/m3/DepositActivity.java
+++ src/com/example/m3/DepositActivity.java
@@ -18,84 +18,114 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+/**
+ * Deposit activity class.
+ * 
+ * @author Tripp
+ * @version 1.0
+ */
 public class DepositActivity extends Activity {
-	
-	/**
-	 * Authentication token string instance variable.
-	 */
-    private String auth_token;
-	private String account_id;
-	private Button depositButton;
-	private EditText source;
-	private EditText effectiveDate;
-	private EditText amount;
+    
+    /**
+     * Authentication token string instance variable.
+     */
+    private String authToken;
+    
+    /**
+     * String accountID.
+     */
+    private String accountID;
+    
+    /**
+     * Button depositButton.
+     */
+    private Button depositButton;
+    
+    /**
+     * EditText source.
+     */
+    private EditText source;
+    
+    /**
+     * EditText effectiveDate.
+     */
+    private EditText effectiveDate;
+    
+    /**
+     * EditText amount.
+     */
+    private EditText amount;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_deposit);
-		Intent oldIntent = getIntent();
-		auth_token = oldIntent.getStringExtra("auth_token");
-        account_id = oldIntent.getStringExtra("account_id");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deposit);
+        Intent oldIntent = getIntent();
+        authToken = oldIntent.getStringExtra("authToken");
+        accountID = oldIntent.getStringExtra("accountID");
         addListenerOnCreateButton();
-	}
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.deposit, menu);
-		return true;
-	}
-	
-	public void updateTextView(String newStr) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.deposit, menu);
+        return true;
+    }
+    
+    /**
+     * Update text view method.
+     * 
+     * @param newStr new string
+     */
+    public void updateTextView(String newStr) {
         TextView textView = (TextView) findViewById(R.id.errors);
         textView.setText(newStr);
     }
-	
-	public void addListenerOnCreateButton() {
-		 
-		final Context context = this;
- 
-		depositButton = (Button) findViewById(R.id.depositButton);
- 
-		depositButton.setOnClickListener(new OnClickListener() {
- 
-			@Override
-			public void onClick(View arg0) {
-				source   = (EditText)findViewById(R.id.source_input);
-				effectiveDate   = (EditText)findViewById(R.id.effective_date_input);
-				amount   = (EditText)findViewById(R.id.amount_input);
-				String sourceStr = source.getText().toString();
-				String effectiveDateStr = effectiveDate.getText().toString();
-				String amountStr = amount.getText().toString();
-				try {
-					HttpResponse response = new DatabaseInterface().deposit(sourceStr, effectiveDateStr, amountStr, account_id, auth_token);
-					if (response.getStatusLine().getStatusCode() == 201 ){
-						Intent intent = new Intent(context, TransPageActivity.class);
-						intent.putExtra("auth_token", auth_token);
-			            intent.putExtra("account_id", account_id);
-						startActivity(intent); 
-					} else {
-						updateTextView(EntityUtils.toString(response.getEntity()));
-					}
-				} catch (InterruptedException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (ExecutionException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				} catch (ParseException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-			}
- 
-		});
     
-	}
+    /**
+     * Adds button listener on buttons.
+     */
+    public void addListenerOnCreateButton() {
+         
+        final Context context = this;
+ 
+        depositButton = (Button) findViewById(R.id.depositButton);
+ 
+        depositButton.setOnClickListener(new OnClickListener() {
+ 
+            @Override
+            public void onClick(View arg0) {
+                source   = (EditText) findViewById(R.id.source_input);
+                effectiveDate   = (EditText) findViewById(R.id.effective_date_input);
+                amount   = (EditText) findViewById(R.id.amount_input);
+                String sourceStr = source.getText().toString();
+                String effectiveDateStr = effectiveDate.getText().toString();
+                String amountStr = amount.getText().toString();
+                try {
+                    HttpResponse response = new DatabaseInterface().deposit(sourceStr, effectiveDateStr, amountStr, accountID, authToken);
+                    if (response.getStatusLine().getStatusCode() == 201 ) {
+                        Intent intent = new Intent(context, TransPageActivity.class);
+                        intent.putExtra("authToken", authToken);
+                        intent.putExtra("accountID", accountID);
+                        startActivity(intent); 
+                    } else {
+                        updateTextView(EntityUtils.toString(response.getEntity()));
+                    }
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                } catch (ExecutionException e1) {
+                    e1.printStackTrace();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                
+            }
+ 
+        });
+    
+    }
 
 }
